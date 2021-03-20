7f5e77bef15a69687440be9a1cc07e445eb92de5
DESKTOP-25L5NE5\小眼先生 <360507245@qq.com>
2021-03-20 23:04:31 +0800

    temp

--- Project/Editor/RenZhaiEditor/Source/RenZhaiEditor.Target.cs
+++ Project/Editor/RenZhaiEditor/Source/RenZhaiEditor.Target.cs
@@ -9,6 +9,6 @@ public class RenZhaiEditorTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "RenZhaiEditor" } );
+		ExtraModuleNames.AddRange( new string[] { "RenZhaiEditor" } ); 
 	}
 }
